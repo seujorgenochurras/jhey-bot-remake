5fa178fa59517fbf9b6f3e1619c36b0b3ad54438
Jorge Seu Churrasco com <eusoulegaldemaiseverdadeeujuro@gmail.com>
2022-12-05 22:29:00 -0300

    Created Jhey Bot

---
+++ src/main/java/JheyBot/Bot.java
@@ -0,0 +1,50 @@
+package JheyBot;
+
+
+
+
+import JheyBot.Events.EventListener;
+import io.github.cdimascio.dotenv.Dotenv;
+import net.dv8tion.jda.api.OnlineStatus;
+import net.dv8tion.jda.api.entities.Activity;
+import net.dv8tion.jda.api.sharding.DefaultShardManagerBuilder;
+import net.dv8tion.jda.api.sharding.ShardManager;
+
+import javax.security.auth.login.LoginException;
+
+
+public class Bot{
+
+   private final ShardManager shardManager;
+   private final Dotenv dotenv;
+
+   public Bot() throws LoginException {
+      dotenv = Dotenv.configure().load();
+
+      DefaultShardManagerBuilder builder = DefaultShardManagerBuilder.createDefault(dotenv.get("TROLLED_BY_JOTINHA"));
+
+      builder.setActivity(Activity.watching("SEU PAI DE CALCINHA"));
+      builder.setStatus(OnlineStatus.ONLINE);
+      shardManager = builder.build();
+
+      //Register Listeners
+      shardManager.addEventListener(new EventListener());
+   }
+
+
+   public ShardManager shardManager(){
+      return shardManager;
+   }
+   public static void main(String[] args) {
+      try {
+
+
+         Bot bot = new Bot();
+      }catch (LoginException e){
+         System.out.println("TOKEN INVALID LOGIN EXCEPTION!!!");
+      }
+
+
+   }
+
+}
---
+++ src/main/java/JheyBot/Events/EventListener.java
@@ -0,0 +1,17 @@
+package JheyBot.Events;
+
+import net.dv8tion.jda.api.entities.User;
+import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+
+public class EventListener extends ListenerAdapter {
+
+   @Override
+   public void onMessageReceived(MessageReceivedEvent event) {
+      User user = event.getAuthor();
+      String message = event.getMessage().getContentRaw();
+      if(message.equals("PINTO GROSSO AHINNNN"))
+      event.getChannel().sendMessage("IAMETER NO CU DA SAI").queue();
+   }
+
+}
