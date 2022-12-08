97a0e9fde4e626a8de28dccc986f68ebf690b6c3
Jorge Seu Churrasco com <eusoulegaldemaiseverdadeeujuro@gmail.com>
2022-12-08 00:57:58 -0300

    made some changes to play
    better code

--- src/main/java/JheyBot/Commands/Play.java
+++ src/main/java/JheyBot/Commands/Play.java
@@ -1,15 +1,10 @@
 package JheyBot.Commands;
 
-import JheyBot.Bot;
-import net.dv8tion.jda.api.entities.Member;
-import net.dv8tion.jda.api.entities.SelfUser;
-import net.dv8tion.jda.api.entities.User;
+import com.sedmelluq.discord.lavaplayer.player.AudioPlayerManager;
+import com.sedmelluq.discord.lavaplayer.source.AudioSourceManagers;
 import net.dv8tion.jda.api.entities.channel.middleman.AudioChannel;
-import net.dv8tion.jda.api.entities.channel.unions.AudioChannelUnion;
-import net.dv8tion.jda.api.events.guild.GuildJoinEvent;
 import net.dv8tion.jda.api.events.guild.GuildReadyEvent;
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
-import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import net.dv8tion.jda.api.interactions.commands.OptionMapping;
 import net.dv8tion.jda.api.interactions.commands.OptionType;
@@ -20,25 +15,17 @@ import net.dv8tion.jda.api.managers.AudioManager;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 public class Play extends ListenerAdapter {
-
-   @Override
-      public void onMessageReceived(MessageReceivedEvent event) {
-      User user = event.getAuthor();
-      String message = event.getMessage().getContentRaw();
-      if (user.isBot()) return;
-
-      if (message.startsWith("!play")) {
-
-      }
-   }
-
-      private void join(Member member){
-         if(member.getVoiceState().inAudioChannel()){
+      private void join(SlashCommandInteractionEvent event){
+         if(!event.getMember().getVoiceState().inAudioChannel()){
+            event.reply("SEU IMBECIL ENTRA NA PORRA DE UM CANAL").queue();
+            return;
          }
+         AudioChannel userChannel = event.getMember().getVoiceState().getChannel();
+         AudioManager audioManager = event.getGuild().getAudioManager();
+         audioManager.openAudioConnection(userChannel);
       }
 
    @Override
@@ -48,13 +35,8 @@ public class Play extends ListenerAdapter {
          OptionMapping optionMapping = event.getOption("query");
          String userTag = event.getUser().getAsTag();
          event.reply("SUGMA DICKKK " + userTag + " " + optionMapping.getAsString()).setEphemeral(true).queue();
-         if(!event.getMember().getVoiceState().inAudioChannel()){
-            event.reply("SEU IMBECIL ENTRA NA PORRA DE UM CANAL").queue();
-            return;
-         }
-         AudioChannel userChannel = event.getMember().getVoiceState().getChannel();
-        AudioManager audioManager = event.getGuild().getAudioManager();
-         audioManager.openAudioConnection(userChannel);
+         join(event);
+
       }
    }
 
@@ -64,10 +46,6 @@ public class Play extends ListenerAdapter {
       OptionData query = new OptionData(OptionType.STRING, "query", "Searches for the music", true);
       commandDataList.add(Commands.slash("play", "Plays youtube stuff").addOptions(query));
       event.getGuild().updateCommands().addCommands(commandDataList).queue();
-   }
-
-   private void joinChannel(){
-
    }
 }
 
