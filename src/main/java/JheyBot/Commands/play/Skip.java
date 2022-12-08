8b92a9783710be727d995e882ab51842655f620d
Jorge Seu Churrasco com <eusoulegaldemaiseverdadeeujuro@gmail.com>
2022-12-08 17:17:59 -0300

    added Skip command
    Bot skips music

--- src/main/java/JheyBot/Commands/play/Skip.java
+++ src/main/java/JheyBot/Commands/play/Skip.java
@@ -1,7 +1,13 @@
 package JheyBot.Commands.play;
 
+import JheyBot.musicHandler.PlayerManager;
+import net.dv8tion.jda.api.events.interaction.command.GenericCommandInteractionEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 
 public class Skip extends ListenerAdapter {
-   
+
+   public static void skipMusic(GenericCommandInteractionEvent event) {
+      PlayerManager.getINSTANCE().getMusicManager(event.getGuild()).schedule.nextTrack();
+   }
+
 }
